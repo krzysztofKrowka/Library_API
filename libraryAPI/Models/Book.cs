5fafaed5fc8d22c49ad3410813e1c6c731a5762a
krzysiek.krowka <krzysztof.krowka@onet.eu>
2023-03-08 11:07:16 +0100

    feat: add Book and BookContext classes

---
+++ libraryAPI/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace libraryAPI.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public double Cost { get; set; }
+        public int PublicationDate { get; set; }
+        public string Category { get; set; }
+    }
+}
